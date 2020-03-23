d9c3f53f4fd25124001372c06d15494c94f04bc3
Neel Redkar <neel.redkar@outlook.com>
2020-03-23 00:29:34 -0700

    oops i included dist

--- dist/schema/Node.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-exports["default"] = new Schema({
-    socketId: String
-});
-//# sourceMappingURL=Node.js.map
